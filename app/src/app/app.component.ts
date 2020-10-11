a31eac72861d64c59597642cf83cff16a8437e43
leonardobussi <leonardobezerrabussy@gmail.com>
2020-10-11 20:49:29 -0300

    teste

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -3,48 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  menuOpened = false;
-
-  listTitles = [{
-      trailer: 'assets/videos/cap.mp4',
-      cover: 'assets/images/guardios-da-galaxia.jpg',
-      name: 'Guardiões da galaxia',
-      relevance: 98,
-      age: 12,
-      parts: 2
-    },{
-      trailer: 'assets/videos/cap.mp4',
-      cover: 'assets/images/guardios-da-galaxia.jpg',
-      name: 'Hora de cagar',
-      relevance: 0,
-      age: 18,
-      parts: 1
-    },{
-      trailer: 'assets/videos/cap.mp4',
-      cover: 'assets/images/guardios-da-galaxia.jpg',
-      name: 'sua maẽ é uma trapper',
-      relevance: 100,
-      age: 0,
-      parts: 16
-    },{
-      trailer: 'assets/videos/cap.mp4',
-      cover: 'assets/images/guardios-da-galaxia.jpg',
-      name: 'sua maẽ é uma trapper 2',
-      relevance: 90,
-      age: 0,
-      parts: 10
-    }
-  ]
-
-  setMenuState(state: boolean){
-    this.menuOpened = state
-  }
-  closeMenu(){
-    this.menuOpened = false
-  }
-
+  title = 'app';
 }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,20 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FullBannerComponent } from './shared/full-banner/full-banner.component';
-import { ListTitlesComponent } from './shared/list-titles/list-titles.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { TitleComponent } from './shared/title/title.component';
-import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent,
-    FullBannerComponent,
-    ListTitlesComponent,
-    FooterComponent,
-    TitleComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
