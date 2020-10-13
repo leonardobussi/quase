35668470504132cdbb4428632b440c1646848202
leonardobussi <leonardobezerrabussy@gmail.com>
2020-10-13 18:33:01 -0300

    up

--- app/src/app/contatos-listagem/contatos-listagem.component.ts
+++ app/src/app/contatos-listagem/contatos-listagem.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ContatoService } from '../contato.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contatos-listagem',
@@ -9,16 +10,20 @@ import { ContatoService } from '../contato.service';
 export class ContatosListagemComponent implements OnInit {
 
   contatos: Array<any[]>;
+  name = 'Angular 6';
+  safeSrc: SafeResourceUrl;
 
 
-  constructor(private contatoService: ContatoService) { }
+  constructor(private contatoService: ContatoService, private sanitizer: DomSanitizer) {
+    this.safeSrc =  this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/ZNSWsDJ-Su0");
+   }
 
   ngOnInit(): void {
     this.listar()
   }
-
   listar(){
     return this.contatoService.listar().subscribe(dados => this.contatos = dados)
   }
+ 
 
 }
